5e4751de7dedd8a5539526fa53896adbc3a5c1d5
Petar Hr <petar.hristozov@outlook.com>
2020-10-29 10:18:05 +0200

    Remove Scaffolded items & Add razor runtime compilation

---
+++ Services/IRecipeService.cs
@@ -0,0 +1,15 @@
+﻿using eDoc.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eDoc.Services
+{
+    public interface  IRecipeService
+    {
+        public IEnumerable<Recipe> GetUserRecipes(string username);
+
+        public void CreateRecipe(string username, string doctor);
+    }
+}
---
+++ Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eDoc.Services
+{
+    public interface IUserService
+    {
+        public void GetAllUsers();
+
+    }
+}
