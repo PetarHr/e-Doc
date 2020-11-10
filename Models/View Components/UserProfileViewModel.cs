ef11f699e293d35c1b87ab9861eba8be654fba91
Petar Hr <petar.hristozov@outlook.com>
2020-11-10 17:40:04 +0200

    Created sidebar view component
    Functiona sidebar view component applied.

---
+++ Models/View Components/UserProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eDoc.Models.View_Components
+{
+    public class UserProfileViewModel
+    {
+        public string FirstName { get; set; }
+        public string FamilyName { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ ViewComponents/UserProfileViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using eDoc.Data;
+using eDoc.Data.Models;
+using eDoc.Models.View_Components;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eDoc.ViewComponents
+{
+    public class UserProfileViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext db;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UserProfileViewComponent(ApplicationDbContext db, UserManager<ApplicationUser> userManager)
+        {
+            this.db = db;
+            this.userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(string user)
+        {
+            var userProfile = await userManager.FindByIdAsync(user);
+            var userRole = userManager.GetRolesAsync(userProfile).GetAwaiter().GetResult().FirstOrDefault();
+
+            var viewModel = new UserProfileViewModel
+            {
+                FirstName = userProfile.FirstName,
+                FamilyName = userProfile.FamilyName,
+                Role = userRole
+            };
+
+            return View(viewModel);
+        }
+    }
+}
